5b07175280f57c8dd386869323f315add3c5f73e
Jake <jake.fay9673@gmail.com>
2021-12-05 16:25:47 -0500

    thought.js

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, 
+      get: timeStamp => dateFormat(timeStamp)
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: []
+  },
+  {
+    toJSON: {
+      virtuals: true, 
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
